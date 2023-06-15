bae454a0acfe40f7e9e906c9ee8090bd8766d25b
yhames <wjddn494@naver.com>
2023-06-15 18:47:06 +0900

    simpleJdbcInsert

--- spring-DB/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ spring-DB/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,6 +1,6 @@
 package hello.itemservice;
 
-import hello.itemservice.config.JdbcTemplateV2Config;
+import hello.itemservice.config.JdbcTemplateV3Config;
 import hello.itemservice.repository.ItemRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +11,8 @@ import org.springframework.context.annotation.Profile;
 
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
-@Import(JdbcTemplateV2Config.class)
+//@Import(JdbcTemplateV2Config.class)
+@Import(JdbcTemplateV3Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")	// 컨트롤러만 자동등록, 나머지 리포지토리와 서비스는 수동등록
 public class ItemServiceApplication {
 
