6bb4eb18c8e29abd121ac64f1e66bf73d181ddca
yhames <wjddn494@naver.com>
2025-05-31 21:06:30 +0900

    feat: 스케쥴러 추가

--- project02/flow/src/main/java/com/fastcampus/flow/service/UserQueueService.java
+++ project02/flow/src/main/java/com/fastcampus/flow/service/UserQueueService.java
@@ -1,14 +1,20 @@
 package com.fastcampus.flow.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuples;
 
 import java.time.Instant;
 
 import static com.fastcampus.flow.execption.ErrorCode.QUEUE_ALREADY_REGISTERED_USER;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserQueueService {
@@ -17,6 +23,11 @@ public class UserQueueService {
 
     private static final String USER_QUEUE_WAIT_KEY = "user:queue:%s:wait";
     private static final String USER_QUEUE_PROCEED_KEY = "user:queue:%s:proceed";
+
+    private static final String USER_QUEUE_WAIT_KEY_FOR_SCAN = "user:queue:*:wait";
+
+    @Value("${scheduler.enabled}")
+    private Boolean scheduled = false;
 
     public Mono<Long> registerWaitQueue(final String queue, final Long userId) {
         long now = Instant.now().getEpochSecond();
@@ -46,5 +57,21 @@ public class UserQueueService {
                 .defaultIfEmpty(-1L)
                 .map(rank -> rank >= 0 ? rank + 1 : rank);
 
+    }
+
+    @Scheduled(initialDelay = 5000, fixedDelay = 3000)   // After 5 seconds, repeat every 3 seconds
+    public void scheduledAllowUser() {
+        if (!scheduled) {
+            log.info("Scheduler is disabled, skipping UserQueueService.scheduledAllowUser");
+            return;
+        }
+
+        var maxAllowUserCount = 10L;
+        redisTemplate.scan(ScanOptions.scanOptions().match(USER_QUEUE_WAIT_KEY_FOR_SCAN).count(100).build())
+                .map(key -> key.split(":")[2])
+                .flatMap(queue -> allowUser(queue, maxAllowUserCount)
+                        .map(allowed -> Tuples.of(queue, allowed)))
+                .doOnNext(tuple -> log.info("Tried allow {} and allowed {} users of {} queue", maxAllowUserCount, tuple.getT2(), tuple.getT1()))
+                .subscribe();
     }
 }
